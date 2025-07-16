bf505a43d882063ebaac55ab10843ff286de5941
Emmanuel M <tdude1994@gmail.com>
2025-07-16 09:32:26 +0200

    new file to be pushed

---
+++ script.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to the greater walls!!!"
+
+echo "Variables checking and strings"
+name="Emmanuel"
+surname="Mothogwane"
+age=40
+db_birth="2025-05-20"
+echo
+echo "What is your name my good Sir: $name and your surname Sir: $surname"
+echo "How old are you Sir: $age and when were you born Sir: $db_birth"
+
